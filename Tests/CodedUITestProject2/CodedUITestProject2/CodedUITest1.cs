cf8f82386b5858c1e36a88b4e3c74c379b1c1a76
Edward Grady <psyeg2@ad.nottingham.ac.uk>
2017-12-01 15:20:18 +0000

    new teste project

---
+++ Tests/CodedUITestProject2/CodedUITestProject2/CodedUITest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+
+
+namespace CodedUITestProject2
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data.csv","data#csv", DataAccessMethod.Sequential), DeploymentItem("data.csv"), TestMethod]
+        public void CodedUITestMethod1()
+        {
+            MessageBox.Show(TestContext.DataRow[0].ToString());
+                
+            
+            this.UIMap.UIForm1Window.UISalaryTextWindow.SearchProperties[WinButton.PropertyNames.Name] =TestContext.DataRow["Salary"].ToString();
+            this.UIMap.UIForm1Window.UIMonthsTextWindow.SearchProperties[WinButton.PropertyNames.Name] =TestContext.DataRow["Months"].ToString();
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.CalculateSalary();
+
+            this.UIMap.TestValue(TestContext.DataRow["Bonus"].ToString());
+
+        }
+        
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
